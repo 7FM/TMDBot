54ffb7d9367a515b7d74b8680ff5e6c9e17ee095
7FM <41307817+7FM@users.noreply.github.com>
2024-02-17 20:06:26 +0100

    package tmdbot

--- flake.nix
+++ flake.nix
@@ -11,41 +11,53 @@
           allowUnfree = true;
         };
       };
+
+      myPyPackages = python-packages: with python-packages; [
+        requests
+        python-telegram-bot
+        autopep8 # autoformatter
+        pyyaml
+        (buildPythonPackage rec {
+          pname = "tmdbv3api";
+          version = "1.9.0";
+
+          src = pkgs.fetchPypi {
+            inherit pname version;
+            sha256 = "sha256-UExdprmcRRb/FgoBV2ES0JfyCcBTT5Q8FcS1bL2Swzs=";
+          };
+
+          propagatedBuildInputs = [
+            requests
+            urllib3
+          ];
+
+          doCheck = false;
+
+          pythonImportsCheck = [ "tmdbv3api" ];
+        })
+      ];
+
+      tmdbot = pkgs.callPackage ({ buildPythonPackage, python-packages }: with python-packages; buildPythonPackage {
+          pname = "tmdbot";
+          version = "0.1";
+          src = ./.;
+          propagatedBuildInputs = myPyPackages;
+          doCheck = false;
+      }) { python-packages = pkgs.python3.pkgs; };
+
+      myPythonWithPackages = pkgs.python3.withPackages myPyPackages;
     in {
-      devShell = with pkgs; let
-        myPyPackages = python-packages: with python-packages; [
-          requests
-          python-telegram-bot
-          autopep8 # autoformatter
-          pyyaml
-          (buildPythonPackage rec {
-            pname = "tmdbv3api";
-            version = "1.9.0";
-
-            src = fetchPypi {
-              inherit pname version;
-              sha256 = "sha256-UExdprmcRRb/FgoBV2ES0JfyCcBTT5Q8FcS1bL2Swzs=";
-            };
-
-            propagatedBuildInputs = [
-              requests
-              urllib3
-            ];
-
-            doCheck = false;
-
-            pythonImportsCheck = [ "tmdbv3api" ];
-          })
-        ];
-
-        myPythonWithPackages = pkgs.python3.withPackages myPyPackages;
-
+      packages = {
+        inherit tmdbot;
+        default = tmdbot;
+      };
       # TODO use stdEnv?
-      in mkShellNoCC {
+      devShell = pkgs.mkShellNoCC {
         nativeBuildInputs = [
           # Python env for the utility scripts & cocotb
           myPythonWithPackages
         ];
-        hardeningDisable = [ "all" ];
       };
-    });}
+    }
+  );
+}
